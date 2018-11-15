42c870bf3330c21cad30984a02db3988a6b2a9a3
Radek Lesner <radek3301linux.pl>
2018-11-15 18:44:39 +0100

    v0.2.2 version project - remove file

--- src/systeminfo.cpp
+++ src/systeminfo.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : systeminfo.cpp
 // Author      : Radek Lesner
-// Version     :
+// Version     : 0.2.2
 // Copyright   : Your copyright notice
 // Description : systeminfo in C++, Ansi-style
 //============================================================================
@@ -9,51 +9,33 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <sys/utsname.h>
+#include <sys/sysinfo.h>
 
 using namespace std;
 
 string osname, kernel, architecture, shell, hostname, uptime;
 
-int main() {
-	system("cd ~/");
-	system("uname -o >> .systeminfo-file.txt");
-	system("uname -r >> .systeminfo-file.txt");
-	system("arch >> .systeminfo-file.txt");
-	system("echo $SHELL >> .systeminfo-file.txt");
-	system("hostname >> .systeminfo-file.txt");
-	system("uptime -p >> .systeminfo-file.txt");
+int main(void) {
 
-	string line;
-		    int nr_line=1;
+	char* Shell;
+	Shell = getenv ("SHELL");
 
-		    fstream file;
-		    file.open(".systeminfo-file.txt", ios::in);
+	struct utsname buffer;
 
-		    if(file.good()==false)
-		    	cout<<"Error 001: Not found file \".systeminfo-file.txt\"";
+	errno = 0;
+		if (uname(&buffer) != 0) {
+			perror("uname");
+			exit(EXIT_FAILURE);
+		}
 
-		    while (getline(file, line)) {
-		        switch (nr_line) {
-		            case 1: osname=line; break;
-		            case 2: kernel=line; break;
-		            case 3: architecture=line; break;
-		            case 4: shell=line; break;
-		            case 5: hostname=line; break;
-		            case 6: uptime=line; break;
-		        }
-		        nr_line++;
-		    }
+	cout << "OS Name:			" << buffer.sysname << endl;
+	cout << "Kernel version:			" << buffer.release << endl;
+	cout << "System architecture:		" << buffer.machine << endl;
+	if (Shell!=NULL) {
+		cout << "Shell:				" << Shell << endl;
+	}
+	cout << "Hostname:			" << buffer.nodename << endl;
 
-		    file.close();
-
-		    cout << "OS Name:			" << osname << endl;
-			cout << "Kernel version:			" << kernel << endl;
-			cout << "System architecture:		" << architecture << endl;
-			cout << "Shell:				" << shell << endl;
-			cout << "Hostname:			" << hostname << endl;
-			cout << "Uptime:				" << uptime << endl;
-
-			system("rm .systeminfo-file.txt");
-
-		    return 0;
+	return 0;
 }
