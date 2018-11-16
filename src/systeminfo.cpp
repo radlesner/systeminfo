9c531a02562f4ac645756c38b50a5011ef479b9a
Radek Lesner <radek3301linux.pl>
2018-11-16 10:38:25 +0100

    v0.2.3 version project - add uptime in file

--- src/systeminfo.cpp
+++ src/systeminfo.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : systeminfo.cpp
 // Author      : Radek Lesner
-// Version     : 0.2.2
+// Version     : 0.2.3
 // Copyright   : Your copyright notice
 // Description : systeminfo in C++, Ansi-style
 //============================================================================
@@ -17,6 +17,28 @@ using namespace std;
 string osname, kernel, architecture, shell, hostname, uptime;
 
 int main(void) {
+	system("cd /systeminfo-files/ && pwd && uptime -p >> systeminfo-file.txt");
+
+	//-----------------------------------------------------------------------------------------------
+	string line;
+				    int nr_line=1;
+
+				    //fstream file;
+				    ifstream file("/systeminfo-files/systeminfo-file.txt");
+				    //file.open(".systeminfo-file.txt", ios::in);
+
+				    if(file.good()==false)
+				    	cout<<"Error 001: Not found file \"systeminfo-file.txt\"";
+
+				    while (getline(file, line)) {
+				        switch (nr_line) {
+				            case 1: uptime=line; break;
+				        }
+				        nr_line++;
+				    }
+
+				    file.close();
+	//-----------------------------------------------------------------------------------------------
 
 	char* Shell;
 	Shell = getenv ("SHELL");
@@ -36,6 +58,9 @@ int main(void) {
 		cout << "Shell:				" << Shell << endl;
 	}
 	cout << "Hostname:			" << buffer.nodename << endl;
+	cout << "Uptime:				" << uptime << endl;
+
+	system("rm /systeminfo-files/systeminfo-file.txt");
 
 	return 0;
 }
