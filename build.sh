c04a331de52baac3a35d146210fa788f9ae6519d
Radek <radek3301@linux.pl>
2018-12-14 12:11:34 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #bin/sh
 printf "Files:\n"
 ls -1 src/*.cpp
-printf "Command: g++ src/*.cpp -o Debug/systeminfo\n"
+printf "Command: make clean & make\n"
 cd Debug && make clean && make
 
 #g++ src/*.cpp -o Debug/systeminfo
