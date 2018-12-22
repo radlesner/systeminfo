e3cd85d21666bdf37baabe64804b58acd473698d
Radek <radek3301@linux.pl>
2018-12-22 11:27:13 +0100

    deleted copy_to_usr_bin.sh

--- copy_to_usr_bin.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-chmod +x systeminfo_command_activate.sh
-chmod +x systeminfo_command_remove.sh
-
-sudo cp Debug/systeminfo				/usr/bin
-sudo cp systeminfo_command_activate.sh	/usr/bin
-sudo cp systeminfo_command_remove.sh	/usr/bin
