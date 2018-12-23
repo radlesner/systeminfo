9475360711c66bb5eae337b4601aca7c0ea8f36f
Radek <radek3301@linux.pl>
2018-12-23 20:36:31 +0100

    added used value in swap function

--- src/functions_file_swap.cpp
+++ src/functions_file_swap.cpp
@@ -13,28 +13,31 @@ void swap_file() {
 	const string input_value = "/systeminfo-files/systeminfo-swap.txt";
 	const int line_1 = 1;
 	const int line_2 = 2;
-	double swap_total, swap_free;
+	double swap_total, swap_free, swap_used;
 
 	string value_1 = open_file(input_value, line_1);
 	string value_2 = open_file(input_value, line_2);
 
 	swap_total	= (double)atoi(value_1.c_str())	/ 1024 / 1000;
 	swap_free	= (double)atoi(value_2.c_str())	/ 1024 / 1000;
+	swap_used	= swap_total - swap_free;
 
-	cout << "Swap Total/Free:		" << swap_total << "/" << swap_free << " GB" << endl;
+	cout << "Swap Total/Free/Used:		" << swap_total << "/" << swap_free << "/" << swap_used << " GB" << endl;
 }
 
 void swap_megabyte_file() {
 	const string input_value = "/systeminfo-files/systeminfo-swap.txt";
 	const int line_1 = 1;
 	const int line_2 = 2;
-	int swap_total, swap_free;
+	int swap_total, swap_free, swap_used;
 
 	string value_1 = open_file(input_value, line_1);
 	string value_2 = open_file(input_value, line_2);
 
 	swap_total	= atoi(value_1.c_str()) / 1024;
 	swap_free	= atoi(value_2.c_str()) / 1024;
+	swap_used	= swap_total - swap_free;
 
-	cout << "Swap Total/Free:		" << swap_total << "/" << swap_free << " MB" << endl;
+
+	cout << "Swap Total/Free/Used:		" << swap_total << "/" << swap_free << "/" << swap_used << " MB" << endl;
 }
