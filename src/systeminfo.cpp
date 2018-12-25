01a06a038b7c044a3c3deb80afbe87a1a78b657d
Radek <radek3301@linux.pl>
2018-12-25 16:01:44 +0100

    v0.7.7 - correction frequency monitor function

--- src/systeminfo.cpp
+++ src/systeminfo.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : systeminfo.cpp
 // Author      : Radek Lesner
-// Version     : 0.7.6
+// Version     : 0.7.7
 // Copyright   : Your copyright notice
 // Description : systeminfo in C++, Ansi-style
 //============================================================================
@@ -14,7 +14,7 @@
 using namespace std;
 
 void output_version() {
-	string version = "0.7.6";
+	string version = "0.7.7";
 	cout << "systeminfo 2018, v" << version << endl;
 	cout << "Compiled: " << __DATE__ << " | " << __TIME__ << " | " << __FILE__ << endl;
 }
