642d93f1ad027a877bdfdbb4326972dbad13d627
Radek <radek3301@linux.pl>
2018-12-25 20:35:43 +0100

    experiment in function cpu only

--- src/functions_output.cpp
+++ src/functions_output.cpp
@@ -85,9 +85,16 @@ void output_ssh_info() {
 void output_cpu_only() {
 	cpu_file();
 	cores_file();
-	if(cpu_frequency() == 1)	cout << "CPU Frequency:			N/A" << endl;
+	if(distribution_file() != "Raspbian") {
+		if(cpu_frequency() == 1)	cout << "CPU Frequency:			N/A" << endl;
+		else {
+			cout << "CPU Frequency:			" << cpu_frequency() << " MHz" << endl;
+			cout << "Max Frequency:			" << cpu_frequency_max() << " MHz" << endl;
+			cout << "Min Frequency:			" << cpu_frequency_min() << " MHz" << endl;
+		}
+	}
 	else {
-		cout << "CPU Frequency:			" << cpu_frequency() << " MHz" << endl;
+		cout << "CPU Frequency:			N/A" << endl;
 		cout << "Max Frequency:			" << cpu_frequency_max() << " MHz" << endl;
 		cout << "Min Frequency:			" << cpu_frequency_min() << " MHz" << endl;
 	}
