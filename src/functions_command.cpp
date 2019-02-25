25282b51eb866b7a8f3e47a44e2004c851070e16
Radek <radek3301@linux.pl>
2019-02-25 19:13:17 +0100

    update command functions

--- src/functions_command.cpp
+++ src/functions_command.cpp
@@ -43,9 +43,46 @@ void command_check_folder_exist() {
 }
 
 void command_activate() {
-	system("./systeminfo_script.sh");
+	ifstream mem_file("/systeminfo-files/stripts/systeminfo_script.sh");
+	if(mem_file.good() == false) {
+		system("cd /systeminfo-files && cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq >> systeminfo-cpu-status.txt");
+		system("cd /systeminfo-files && lscpu | grep -i \"max\" --max-count=1 | awk {'print $4'} >> systeminfo-cpu-frequency_max.txt");
+		system("cd /systeminfo-files && lscpu | grep -i \"min\" --max-count=1 | awk {'print $4'} >> systeminfo-cpu-frequency_min.txt");
+		system("cd /systeminfo-files && cat /proc/cpuinfo | grep -i \"model name\" --max-count=1 | awk '{for (i=4; i<NF; i++) printf $i \" \"; print $NF}' >> systeminfo-cpu.txt");
+		system("cd /systeminfo-files && lscpu | grep -i \"CPU(s):\" --max-count=1 | awk {'print $2'}  >> systeminfo-cores.txt");
+
+		system("cd /systeminfo-files && uptime -p | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}' >> systeminfo-uptime.txt");
+		system("cd /systeminfo-files && lsb_release -i | awk {'print $3'} >> systeminfo-distro.txt");
+		system("cd /systeminfo-files && lsb_release -r | awk {'print $2'} >> systeminfo-distro.txt");
+		system("cd /systeminfo-files && arch >> systeminfo-distro.txt");
+		system("cd /systeminfo-files && getconf LONG_BIT >> systeminfo-distro.txt");
+		system("cd /systeminfo-files && echo $USER >> systeminfo-user.txt");
+		system("cd /systeminfo-files && echo $SHELL >> systeminfo-shell.txt");
+
+		system("cd /systeminfo-files && cat /proc/meminfo | grep -i \"MemTotal: \" --max-count=1 | awk {'print $2'} | tr -d 'kB' >> systeminfo-mem.txt");
+		system("cd /systeminfo-files && cat /proc/meminfo | grep -i \"MemFree: \" --max-count=1 | awk {'print $2'} | tr -d 'kB' >> systeminfo-mem.txt");
+		system("cd /systeminfo-files && cat /proc/meminfo | grep -i \"MemAvailable:\" --max-count=1 | awk {'print $2'} >> systeminfo-mem.txt");
+		system("cd /systeminfo-files && cat /proc/meminfo | grep -i \"SwapTotal: \" --max-count=1 | awk {'print $2'} | tr -d 'kB' >> systeminfo-swap.txt");
+		system("cd /systeminfo-files && cat /proc/meminfo | grep -i \"SwapFree: \" --max-count=1 | awk {'print $2'} | tr -d 'kB' >> systeminfo-swap.txt");
+	}
+	else system("cd /systeminfo-files/scripts && ./systeminfo_remove_script.sh");
 }
 
 void command_remove() {
-	system("./systeminfo_remove_script.sh");
+	ifstream mem_file("/systeminfo-files/stripts/systeminfo_script.sh");
+	if(mem_file.good() == false) {
+		system("cd /systeminfo-files && rm systeminfo-uptime.txt >> systeminfo-errors.txt");
+		system("cd /systeminfo-files && rm systeminfo-distro.txt >> systeminfo-errors.txt");
+		system("cd /systeminfo-files && rm systeminfo-shell.txt >> systeminfo-errors.txt");
+		system("cd /systeminfo-files && rm systeminfo-user.txt >> systeminfo-errors.txt");
+
+		system("cd /systeminfo-files && rm systeminfo-mem.txt >> systeminfo-errors.txt");
+		system("cd /systeminfo-files && rm systeminfo-swap.txt >> systeminfo-errors.txt");
+
+		system("cd /systeminfo-files && rm systeminfo-cpu.txt >> systeminfo-errors.txt");
+		system("cd /systeminfo-files && rm systeminfo-cpu-status.txt >> systeminfo-errors.txt");
+		system("cd /systeminfo-files && rm systeminfo-cores.txt >> systeminfo-errors.txt");
+		system("cd /systeminfo-files && rm systeminfo-cpu-frequency*.txt >> systeminfo-errors.txt");
+	}
+	else system("cd /systeminfo-files/scripts && ./systeminfo_remove_script.sh");
 }
