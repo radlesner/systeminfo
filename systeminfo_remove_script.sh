a9595b1a61f460c6c7a1dbec3728675b8f4b9bf3
Radek <radek3301@linux.pl>
2019-03-03 13:16:37 +0100

    update script file

--- systeminfo_remove_script.sh
+++ systeminfo_remove_script.sh
@@ -1,13 +1,16 @@
 cd /systeminfo-files
+# SYSTEM
 rm systeminfo-uptime.txt >> systeminfo-errors.txt
 rm systeminfo-distro.txt >> systeminfo-errors.txt
 rm systeminfo-arch.txt >> systeminfo-errors.txt
 rm systeminfo-shell.txt >> systeminfo-errors.txt
 rm systeminfo-user.txt >> systeminfo-errors.txt
 
+# MEMOWY
 rm systeminfo-mem.txt >> systeminfo-errors.txt
 rm systeminfo-swap.txt >> systeminfo-errors.txt
 
+# CPU
 rm systeminfo-cpu.txt >> systeminfo-errors.txt
 rm systeminfo-cpu-status.txt >> systeminfo-errors.txt
 rm systeminfo-cores.txt >> systeminfo-errors.txt
--- systeminfo_script.sh
+++ systeminfo_script.sh
@@ -1,4 +1,6 @@
 cd /systeminfo-files
+
+# CPU
 cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq >> systeminfo-cpu-status.txt
 lscpu | grep -i "max" --max-count=1 | awk {'print $4'} >> systeminfo-cpu-frequency_max.txt
 lscpu | grep -i "min" --max-count=1 | awk {'print $4'} >> systeminfo-cpu-frequency_min.txt
@@ -6,6 +8,7 @@ cat /proc/cpuinfo | grep -i "model name" --max-count=1 | awk '{for (i=4; i<NF; i
 lscpu | grep -i "CPU(s):" --max-count=1 | awk {'print $2'}  >> systeminfo-cores.txt
 cat /proc/cpuinfo | grep -i "cpu cores" --max-count=1 | awk {'print $4'}  >> systeminfo-cores.txt
 
+# SYSTEM
 uptime -p | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}' >> systeminfo-uptime.txt
 lsb_release -i | awk {'print $3'} >> systeminfo-distro.txt
 lsb_release -r | awk {'print $2'} >> systeminfo-distro.txt
@@ -15,6 +18,7 @@ getconf LONG_BIT >> systeminfo-arch.txt
 echo $USER >> systeminfo-user.txt
 echo $SHELL >> systeminfo-shell.txt
 
+# MENORY
 cat /proc/meminfo | grep -i "MemTotal: " --max-count=1 | awk {'print $2'} | tr -d 'kB' >> systeminfo-mem.txt
 cat /proc/meminfo | grep -i "MemFree: " --max-count=1 | awk {'print $2'} | tr -d 'kB' >> systeminfo-mem.txt
 cat /proc/meminfo | grep -i "MemAvailable:" --max-count=1 | awk {'print $2'} >> systeminfo-mem.txt
