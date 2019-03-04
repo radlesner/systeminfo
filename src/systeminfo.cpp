ea914b23b001426a92b70ded7207cfd7efa7f2fe
Radek <radek3301@linux.pl>
2019-03-04 10:10:42 +0100

    remove bugs

--- src/systeminfo.cpp
+++ src/systeminfo.cpp
@@ -20,12 +20,16 @@ int main(int argc, char** argv) {
 	command_activate();
 	if(argc > 1) {
 		if(!strcmp(argv[1], "-g") || !strcmp(argv[1], "--gigabyte")) {
+			separator("");
 			mem_file(0);
 			swap_file(0);
+			separator("");
 		}
 		else if(!strcmp(argv[1], "-m") || !strcmp(argv[1], "--megabyte")) {
+			separator("");
 			mem_megabyte_file();
 			swap_megabyte_file();
+			separator("");
 		}
 		else if(!strcmp(argv[1], "-s") || !strcmp(argv[1], "--system"))				output_system();
 		else if(!strcmp(argv[1], "-c") || !strcmp(argv[1], "--cpu"))				output_cpu_only();
