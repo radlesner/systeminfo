ced3fce6b1605e50c3d69b96e30e69950a696212
Radek <radek3301@linux.pl>
2019-04-25 12:20:50 +0200

    remove error file

--- systeminfo_remove_script.sh
+++ systeminfo_remove_script.sh
@@ -1,23 +1,23 @@
 cd /$HOME/.systeminfo-files
 # SYSTEM
-rm -f systeminfo-uptime.txt >> systeminfo-errors.txt
-rm -f systeminfo-distro.txt >> systeminfo-errors.txt
-rm -f systeminfo-arch.txt >> systeminfo-errors.txt
-rm -f systeminfo-shell.txt >> systeminfo-errors.txt
-rm -f systeminfo-user.txt >> systeminfo-errors.txt
+rm -f systeminfo-uptime.txt
+rm -f systeminfo-distro.txt
+rm -f systeminfo-arch.txt
+rm -f systeminfo-shell.txt
+rm -f systeminfo-user.txt
 
 # NETWORK
-rm -f systeminfo-gateway-names.txt >> systeminfo-errors.txt
-rm -f systeminfo-gateway-ip.txt >> systeminfo-errors.txt
+rm -f systeminfo-gateway-names.txt
+rm -f systeminfo-gateway-ip.txt
 
 # MEMOWY
-rm -f systeminfo-mem.txt >> systeminfo-errors.txt
-rm -f systeminfo-swap.txt >> systeminfo-errors.txt
+rm -f systeminfo-mem.txt
+rm -f systeminfo-swap.txt
 
 # CPU
-rm -f systeminfo-cpu-status.txt >> systeminfo-errors.txt
-rm -f systeminfo-cpu-frequency_max.txt >> systeminfo-errors.txt
-rm -f systeminfo-cpu-frequency_min.txt >> systeminfo-errors.txt
-rm -f systeminfo-cpu.txt >> systeminfo-errors.txt
-rm -f systeminfo-cores.txt >> systeminfo-errors.txt
-rm -f systeminfo-cpu-frequency*.txt >> systeminfo-errors.txt
+rm -f systeminfo-cpu-status.txt
+rm -f systeminfo-cpu-frequency_max.txt
+rm -f systeminfo-cpu-frequency_min.txt
+rm -f systeminfo-cpu.txt
+rm -f systeminfo-cores.txt
+rm -f systeminfo-cpu-frequency*.txt
