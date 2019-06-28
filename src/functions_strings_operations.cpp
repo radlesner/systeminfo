39a90d56a9e95487ecaaacaa26e48d88330d577a
Radek <radek3301@linux.pl>
2019-06-28 07:19:03 +0200

    v3.1

--- src/functions_strings_operations.cpp
+++ src/functions_strings_operations.cpp
@@ -55,18 +55,18 @@ string get_color(string color_input)
 {
     string color;
 
-    if (color_input == "black")              color = "\033[0m\033[1;30m";
-    else if (color_input == "red")           color = "\033[0m\033[1;31m";
-    else if (color_input == "green")         color = "\033[0m\033[1;32m";
-    else if (color_input == "brown")         color = "\033[0m\033[1;33m";
-    else if (color_input == "blue")          color = "\033[0m\033[1;34m";
-    else if (color_input == "cyan")          color = "\033[0m\033[1;36m";
-    else if (color_input == "purple")        color = "\033[0m\033[1;35m";
-    else if (color_input == "yellow")        color = "\033[0m\033[1;33m";
-    else if (color_input == "white")         color = "\033[0m\033[1;37m";
-    else if (color_input == "grey")          color = "\033[0m\033[37m";
-    else if (color_input == "rosa_blue")     color = "\033[01;38;05;25m";
-    else                                     color = "\033[0m\033[1;30m";
+    if (color_input == "black")            color = "\033[0m\033[1;30m";
+    else if (color_input == "red")         color = "\033[0m\033[1;31m";
+    else if (color_input == "green")       color = "\033[0m\033[1;32m";
+    else if (color_input == "brown")       color = "\033[0m\033[1;33m";
+    else if (color_input == "blue")        color = "\033[0m\033[1;34m";
+    else if (color_input == "cyan")        color = "\033[0m\033[1;36m";
+    else if (color_input == "purple")      color = "\033[0m\033[1;35m";
+    else if (color_input == "yellow")      color = "\033[0m\033[1;33m";
+    else if (color_input == "white")       color = "\033[0m\033[1;37m";
+    else if (color_input == "grey")        color = "\033[0m\033[37m";
+    else if (color_input == "rosa_blue")   color = "\033[01;38;05;25m";
+    else                                   color = "\033[0m\033[1;30m";
 
     return color;
 }
