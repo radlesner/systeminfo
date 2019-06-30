d17ad31b95a71dec13a984f8269dd7485f7aba12
Radek <radek3301@linux.pl>
2019-06-30 18:41:16 +0200

    update script

--- systeminfo_script.sh
+++ systeminfo_script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+trim_quotes() {
+    trim_output="${1//\"}"
+    trim_output="${trim_output//\"}"
+    printf "%s" "$trim_output"
+}
+
 cd /$HOME/.systeminfo-files
 
 # CPU
@@ -56,13 +62,124 @@ fi
 
 
 # SYSTEM
+if [[ -f /bedrock/etc/bedrock-release && $PATH == */bedrock/cross/* ]]; then
+    case "$distro_shorthand" in
+        "on" | "tiny") distro="Bedrock Linux" ;;
+        *) distro="$(< /bedrock/etc/bedrock-release)"
+    esac
+elif [[ -f "/etc/redstar-release" ]]; then
+    case "$distro_shorthand" in
+        "on" | "tiny") distro="Red Star OS" ;;
+        *) distro="Red Star OS $(awk -F'[^0-9*]' '$0=$2' /etc/redstar-release)"
+    esac
+
+elif [[ -f "/etc/siduction-version" ]]; then
+    case "$distro_shorthand" in
+        "on" | "tiny") distro="Siduction" ;;
+        *) distro="Siduction ($(lsb_release -sic))"
+    esac
+
+elif type -p lsb_release >/dev/null; then
+    case "$distro_shorthand" in
+        "on")   lsb_flags="-sir" ;;
+        "tiny") lsb_flags="-si" ;;
+        *)      lsb_flags="-sd" ;;
+    esac
+    distro="$(lsb_release "$lsb_flags")"
+
+elif [[ -f "/etc/GoboLinuxVersion" ]]; then
+    case "$distro_shorthand" in
+        "on" | "tiny") distro="GoboLinux" ;;
+        *) distro="GoboLinux $(< /etc/GoboLinuxVersion)"
+    esac
+
+elif type -p guix >/dev/null; then
+    case "$distro_shorthand" in
+        "on" | "tiny") distro="GuixSD" ;;
+        *) distro="GuixSD $(guix system -V | awk 'NR==1{printf $5}')"
+    esac
+
+elif type -p crux >/dev/null; then
+    distro="$(crux)"
+    case "$distro_shorthand" in
+        "on")   distro="${distro//version}" ;;
+        "tiny") distro="${distro//version*}" ;;
+    esac
+
+elif type -p tazpkg >/dev/null; then
+    distro="SliTaz $(< /etc/slitaz-release)"
+
+elif type -p kpt >/dev/null && \
+     type -p kpm >/dev/null; then
+    distro="KSLinux"
+
+elif [[ -d "/system/app/" && -d "/system/priv-app" ]]; then
+    distro="Android $(getprop ro.build.version.release)"
+
+elif [[ -f /etc/lsb-release && "$(< /etc/lsb-release)" == *CHROMEOS* ]]; then
+    distro="$(awk -F '=' '/NAME|VERSION/ {printf $2 " "}' /etc/lsb-release)"
+
+elif [[ -f "/etc/os-release" || \
+        -f "/usr/lib/os-release" || \
+        -f "/etc/openwrt_release" ]]; then
+    files=("/etc/os-release" "/usr/lib/os-release" "/etc/openwrt_release")
+
+    # Source the os-release file
+    for file in "${files[@]}"; do
+        source "$file" && break
+    done
+
+    # Format the distro name.
+    case "$distro_shorthand" in
+        "on")   distro="${NAME:-${DISTRIB_ID}} ${VERSION_ID:-${DISTRIB_RELEASE}}" ;;
+        "tiny") distro="${NAME:-${DISTRIB_ID:-${TAILS_PRODUCT_NAME}}}" ;;
+        "off")  distro="${PRETTY_NAME:-${DISTRIB_DESCRIPTION}} ${UBUNTU_CODENAME}" ;;
+    esac
+else
+    for release_file in /etc/*-release; do
+        distro+="$(< "$release_file")"
+    done
+
+    if [[ -z "$distro" ]]; then
+        case "$distro_shorthand" in
+            "on" | "tiny") distro="$kernel_name" ;;
+            *) distro="$kernel_name $kernel_version" ;;
+        esac
+        distro="${distro/DragonFly/DragonFlyBSD}"
+
+        # Workarounds for FreeBSD based distros.
+        [[ -f "/etc/pcbsd-lang" ]] && distro="PCBSD"
+        [[ -f "/etc/trueos-lang" ]] && distro="TrueOS"
+
+        # /etc/pacbsd-release is an empty file
+        [[ -f "/etc/pacbsd-release" ]] && distro="PacBSD"
+    fi
+fi
+
+if [[ "$(< /proc/version)" == *"Microsoft"* ||
+      "$kernel_version" == *"Microsoft"* ]]; then
+    case "$distro_shorthand" in
+        "on")   distro+=" [Windows 10]" ;;
+        "tiny") distro="Windows 10" ;;
+        *)      distro+=" on Windows 10" ;;
+    esac
+
+elif [[ "$(< /proc/version)" == *"chrome-bot"* || -f "/dev/cros_ec" ]]; then
+    case "$distro_shorthand" in
+        "on")   distro+=" [Chrome OS]" ;;
+        "tiny") distro="Chrome OS" ;;
+        *)      distro+=" on Chrome OS" ;;
+    esac
+fi
+
+distro="$(trim_quotes "$distro")"
+distro="${distro/NAME=}"
+echo $distro >> systeminfo-distro.txt
+
 uptime -p | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}' >> systeminfo-uptime.txt
 if [ -e /etc/lsb-release ] ; then
-    lsb_release -i | awk {'print $3'} >> systeminfo-distro.txt
     lsb_release -r | awk {'print $2'} >> systeminfo-release.txt
     lsb_release -c | awk {'print $2'} >> systeminfo-codename.txt
-else
-    cat /etc/os-release | grep "NAME" | sort -n | tr -d '"' | awk -F '=' '{print $2}' >> systeminfo-distro.txt
 fi
 
 if [ -e /etc/os-release ] ; then
