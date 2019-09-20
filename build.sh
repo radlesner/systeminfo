d8e406083206e2fbaa7d2b9fc457bf6ab4745f17
Radek <radek3301@linux.pl>
2019-09-20 14:02:25 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 printf "Files:\n"
 ls -1 src/*.cpp
+wc -l src/*.cpp | grep -i "total" | awk '{printf $1}'
+printf " lines\n\n"
 
 printf "Command:\n"
 if [ -e systeminfo ] ; then
-    printf "@ make clean\n"
+    printf "$ make clean\n"
     make clean
 fi
-printf "@ make\n"
+printf "$ make\n"
 make
 
 #g++ src/*.cpp -o Debug/systeminfo
